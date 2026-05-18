77fdb95f0712622259c5600dd9ad99298f125554
XYccWA <zzb2888988@outlook.com>
2026-05-18 16:11:51 +0800

    1

---
+++ src/main/java/org/xyccwa/space_simulation/SpaceSimulation.java
@@ -0,0 +1,14 @@
+package org.xyccwa.space_simulation;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(SpaceSimulation.MODID)
+public class SpaceSimulation {
+    public static final String MODID = "space_simulation";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+}
